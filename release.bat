1d5bea9a27634ea96804c099508b02ed0f938f47
highsource <aleksei.valikov@gmail.com>
2014-02-24 00:50:07 +0100

    Version 2.0.0.

--- release.bat
+++ release.bat
@@ -1,3 +1,8 @@
-call mvn clean install
-call mvn release:prepare
-call mvn release:perform
+call mvn --batch-mode release:update-versions -DdevelopmentVersion=%1
+call mvn clean install -DperformRelease
+rem  -Psamples -Ptests -Pdist
+call mvn scm:commit -M="Version %1"
+call mvn scm:tag -Dtag=%1
+call mvn -DperformRelease -Psonatype-oss-release clean deploy
+rem  -Psamples -Ptests -Pdist
+call mvn --batch-mode release:update-versions -DdevelopmentVersion=%2
