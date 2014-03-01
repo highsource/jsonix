93a9aa95c834c11172e24c76872498c852cf4201
highsource <aleksei.valikov@gmail.com>
2014-03-01 16:01:02 +0100

    =Version 2.0.1

--- release.bat
+++ release.bat
@@ -2,7 +2,7 @@ call mvn versions:set -DnewVersion=%1
 pause
 call mvn versions:commit
 pause
-rem call mvn clean install -DperformRelease
+call mvn clean install -DperformRelease
 pause
 rem  -Psamples -Ptests -Pdist
 git commit -a -m="Version %1"
@@ -30,5 +30,6 @@ cd ..
 rem  -Psamples -Ptests -Pdist
 call mvn versions:set -DnewVersion=%2
 call mvn versions:commit
-call mvn scm:commit -M="Version %2"
+call mvn clean install -DperformRelease
+git commit -a -m="Version %1"
 pause
