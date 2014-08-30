0e26186959440114263862291fca509a9ac7855a
highsource <aleksei.valikov@gmail.com>
2014-08-31 00:12:50 +0200

    Updated the WPS example.

--- nodejs/tests/wps/tests/wps-tests.js
+++ nodejs/tests/wps/tests/wps-tests.js
@@ -2,6 +2,26 @@ var Jsonix = require('jsonix').Jsonix;
 var OWS_V_1_1_0 = require('../mappings/OWS_V_1_1_0').OWS_V_1_1_0;
 var WPS_V_1_0_0 = require('../mappings/WPS_V_1_0_0').WPS_V_1_0_0;
 
+function roundtrip(test, resource) {
+	var context = new Jsonix.Context([ OWS_V_1_1_0, WPS_V_1_0_0 ]);
+	var unmarshallerOne = context.createUnmarshaller();
+	var unmarshallerTwo = context.createUnmarshaller();
+	var marshallerOne = context.createMarshaller();
+	var marshallerTwo = context.createMarshaller();
+	console.log('Unmarshalling [' + resource + '].');
+	unmarshallerOne.unmarshalFile(resource,
+		function(one) {
+			console.log('Unmarshalled:');
+			console.log(one);
+			var documentOne = marshallerOne.marshalDocument(one);
+			var two = unmarshallerTwo.unmarshalDocument(documentOne);
+			var stringTwo = marshallerTwo.marshalString(two);
+			console.log(stringTwo);
+			test.ok(Jsonix.Util.Type.isEqual(one, two, function(text) {console.log(text)}), 'Roundtrip [' + resource + '] failed in phase two. Objects must be equal.');
+			test.done();
+		}
+	);
+}
 module.exports = {
 	"UnmarshalFile": function(test) {
 
@@ -22,5 +42,8 @@ module.exports = {
 				test.equal('Buffer a polygon feature', wpsProcess.title.value);
 				test.done();
 		});
-        }
+        },
+	"Roundtrips" : {
+		"execute-01.xml" : function(test) {roundtrip(test, "tests/execute-01.xml");}
+	}
 };
