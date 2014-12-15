8de7f1c7f259845020c15c1a5580f7e7e85ae387
highsource <aleksei.valikov@gmail.com>
2014-12-15 22:46:15 +0100

    Fixed a global sourceIsEvt variable.

--- scripts/src/main/javascript/org/hisrc/jsonix/Jsonix/Util.js
+++ scripts/src/main/javascript/org/hisrc/jsonix/Jsonix/Util.js
@@ -25,7 +25,7 @@ Jsonix.Util.extend = function(destination, source) {
 
 		// REWORK
 		// Node.js
-		var sourceIsEvt = typeof window !== 'undefined' && window !== null && typeof window.Event == "function" && source instanceof window.Event;
+		sourceIsEvt = typeof window !== 'undefined' && window !== null && typeof window.Event === "function" && source instanceof window.Event;
 
 		if (!sourceIsEvt && source.hasOwnProperty && source.hasOwnProperty('toString')) {
 			destination.toString = source.toString;
