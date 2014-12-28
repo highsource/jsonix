8317ed3bec9972112d593285ef59bf611babeb0e
highsource <aleksei.valikov@gmail.com>
2014-12-28 09:55:15 +0100

    Version 2.1.1

---
+++ qprerelease.bat
@@ -0,0 +1,63 @@
+setlocal
+echo Setting JAVA_HOME to %JAVA6_HOME%.
+set JAVA_HOME=%JAVA6_HOME%
+
+echo Performing a short clean build.
+rem pause
+call mvn clean install -DperformRelease
+echo Short clean build completed.
+rem pause
+
+echo Performing a full clean build.
+rem pause
+call mvn clean install -DperformRelease -Pall
+echo Full clean build completed.
+rem pause
+
+echo Setting new version to %1.
+rem pause
+call mvn versions:set -Pall -DnewVersion=%1
+echo Version was set to %1.
+rem pause
+call mvn versions:commit -Pall
+echo Version %1 committed.
+rem pause
+
+echo Performing a short clean build.
+rem pause
+call mvn clean install -DperformRelease
+echo Short clean build completed.
+rem pause
+
+echo Performing a full clean build.
+rem pause
+call mvn clean install -Pall -DperformRelease
+echo Full clean build completed.
+rem pause
+
+echo Checking in version %1.
+rem pause
+git commit -a -m "Version %1"
+echo Version %1 was checked in.
+rem pause
+
+echo Tagging version %1.
+rem pause
+git tag -a %1 -m "Version %1"
+echo Version %1 was tagged.
+rem pause
+
+echo Pushing version %1.
+rem pause
+git push origin master
+git push --tags origin master
+echo Version %1 was pushed.
+rem pause
+
+echo Performing full clean deploy.
+rem pause
+call mvn -DperformRelease -Psonatype-oss-release,all clean install
+echo Full clean deploy done.
+rem pause
+
+endlocal
