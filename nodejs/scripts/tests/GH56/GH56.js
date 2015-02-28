6208a10a3cbc6a8067c374cb6ff16174a5dfd78f
highsource <aleksei.valikov@gmail.com>
2015-02-28 09:34:02 +0100

    Issue #56. Docs.

--- nodejs/scripts/tests/GH56/GH56.js
+++ nodejs/scripts/tests/GH56/GH56.js
@@ -1,21 +1,21 @@
-var Jsonix = require('../../jsonix').Jsonix;
-var roundtrips = require('../roundtrip').roundtrips;
-var comparisons = require('../comparison').comparisons;
+var Jsonix = require("../../jsonix").Jsonix;
+var roundtrips = require("../roundtrip").roundtrips;
+var comparisons = require("../comparison").comparisons;
 
-var Zero = require('./Mappings.js').Zero;
+var Zero = require("./Mappings.js").Zero;
 
 var mappings = [Zero];
 
-var simplifiedContext = new Jsonix.Context(mappings, { mappingStyle : 'simplified' });
-var standardContext = new Jsonix.Context(mappings, { mappingStyle : 'standard' });
+var simplifiedContext = new Jsonix.Context(mappings, { mappingStyle : "simplified" });
+var standardContext = new Jsonix.Context(mappings, { mappingStyle : "standard" });
 
 module.exports = {
 	"Standard" : {
 		"Roundtrips" : roundtrips(__dirname, [standardContext]),
-		"Comparisons" : comparisons(__dirname, [standardContext, '.standard.json'])
+		"Comparisons" : comparisons(__dirname, [standardContext, ".standard.json"])
 	},
 	"Simplified" : {
 		"Roundtrips" : roundtrips(__dirname, [simplifiedContext]),
-		"Comparisons" : comparisons(__dirname, [simplifiedContext, '.simplified.json'])
+		"Comparisons" : comparisons(__dirname, [simplifiedContext, ".simplified.json"])
 	}
 };
