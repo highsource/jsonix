aed9e23deb0d62479f771498a582624418c5c3b9
highsource <aleksei.valikov@gmail.com>
2015-03-24 23:58:32 +0100

    New version for tests.

--- dist/Jsonix-all.js
+++ dist/Jsonix-all.js
@@ -79,9 +79,12 @@ Jsonix.Class = function() {
 };
 
 Jsonix.XML = {
+		BUILTIN_NAMESPACE_PREFIXES : {
+			'xmlns' : 'http://www.w3.org/2000/xmlns/',
+			'xml' : 'http://www.w3.org/XML/1998/namespace' 
+		},
 		XMLNS_NS : 'http://www.w3.org/2000/xmlns/',
 		XMLNS_P : 'xmlns'
-		
 };
 
 
@@ -1590,6 +1593,11 @@ Jsonix.XML.Output = Jsonix.Class({
 	},
 	declareNamespace : function (ns, p)
 	{
+		if (Jsonix.XML.BUILTIN_NAMESPACE_PREFIXES[p])
+		{
+			// If this is a builtin namespace prefix like 'xml' or 'xmlns', do not declare it
+			return;
+		}
 		var index = this.pns.length - 1;
 		var pnsItem = this.pns[index];
 		var reference;
