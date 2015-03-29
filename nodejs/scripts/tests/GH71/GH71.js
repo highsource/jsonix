f25991860d8c4a65dac5526d1af384a9289c14ee
highsource <aleksei.valikov@gmail.com>
2015-03-29 22:14:59 +0200

    Test for issue #71, the error is reproduced.

---
+++ nodejs/scripts/tests/GH71/GH71.js
@@ -0,0 +1,15 @@
+var Jsonix = require("../../jsonix").Jsonix;
+var GH71 = require("./Mappings.js").GH71;
+
+module.exports = {
+	"EmptyElement" : function(test) {
+		var context = new Jsonix.Context([GH71]);
+		var unmarshaller = context.createUnmarshaller();
+		var marshaller = context.createMarshaller();
+		var xml = "<Configuration><Install><ConfigFile>config.xml</ConfigFile><InstallDir></InstallDir><DataDir/></Install></Configuration>";
+		var unmarshalled = unmarshaller.unmarshalString(xml);
+		var marshalled = marshaller.marshalString(unmarshalled);
+		test.equal(xml, marshalled);
+		test.done();
+	}
+};
---
+++ nodejs/scripts/tests/GH71/Mappings.js
@@ -0,0 +1,38 @@
+var GH71 = {
+   name: 'ConfigModel',
+   typeInfos: [{
+       localName: 'Configuration.Install',
+       propertyInfos: [{
+           name: 'configFile',
+           elementName: {
+             localPart: 'ConfigFile'
+           }
+         }, {
+           name: 'installDir',
+           elementName: {
+             localPart: 'InstallDir',
+           }
+         }, {
+           name: 'dataDir',
+           elementName: {
+             localPart: 'DataDir'
+           }
+         }]
+     }, {
+       localName: 'Configuration',
+       propertyInfos: [{
+           name: 'install',
+           elementName: {
+             localPart: 'Install'
+           },
+           typeInfo: '.Configuration.Install'
+         }]
+     }],
+   elementInfos: [{
+       elementName: {
+         localPart: 'Configuration'
+       },
+       typeInfo: '.Configuration'
+     }]
+ };
+module.exports.GH71 = GH71;
