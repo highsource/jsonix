9ed8c3bee44341ec15df0d889be3d065525a7a59
highsource <aleksei.valikov@gmail.com>
2015-04-01 09:21:57 +0200

    Issue #70. Issue #72.

--- scripts/src/main/javascript/org/hisrc/jsonix/Jsonix/Schema/XSI.js
+++ scripts/src/main/javascript/org/hisrc/jsonix/Jsonix/Schema/XSI.js
@@ -1,6 +1,8 @@
 Jsonix.Schema.XSI = {};
 Jsonix.Schema.XSI.NAMESPACE_URI = 'http://www.w3.org/2001/XMLSchema-instance';
 Jsonix.Schema.XSI.PREFIX = 'xsi';
+Jsonix.Schema.XSI.TYPE = 'type';
+Jsonix.Schema.XSI.NIL = 'nil';
 Jsonix.Schema.XSI.qname = function(localPart) {
 	Jsonix.Util.Ensure.ensureString(localPart);
 	return new Jsonix.XML.QName(Jsonix.Schema.XSI.NAMESPACE_URI, localPart,
