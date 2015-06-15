205d901de0869a1cf21e399cf2b440275107a8d7
tom <tom@toms-toy.de>
2015-06-15 14:05:22 +0200

    YearMonth.print & Tests in extra file

--- nodejs/scripts/tests/GH73/GH73.js
+++ nodejs/scripts/tests/GH73/GH73.js
@@ -7,6 +7,7 @@ module.exports = {
 		// "GH73DateTimezoneOffset" : require('./GH73DateTimezoneOffset'),
 		"GH73Calendar" : require('./GH73Calendar'),
 		"GH73GYear" : require('./GH73GYear'),
+		"GH73GYearMonth" : require('./GH73GYearMonth'),
 		"GH73GMonth" : require('./GH73GMonth'),
 		"GH73GDay" : require('./GH73GDay'),
 		"GH73Regex" : require('./GH73Regex')
--- scripts/src/main/javascript/org/hisrc/jsonix/Jsonix/Schema/XSD/GYearMonth.js
+++ scripts/src/main/javascript/org/hisrc/jsonix/Jsonix/Schema/XSD/GYearMonth.js
@@ -4,38 +4,45 @@ Jsonix.Schema.XSD.GYearMonth = Jsonix.Class(Jsonix.Schema.XSD.Calendar, {
 	CLASS_NAME : 'Jsonix.Schema.XSD.GYearMonth',
 
 	parse : function(value, context, input, scope) {
-		var returnValue = this.splitGYearMonth(value);
-		returnValue.toString = function() {
-			return "EmptyXMLElement. Call embedded 'year', 'month' or 'timezone' property";
-		};
-
-		return returnValue;
-	},
-
-	/**
-	 * @param {string}
-	 *            yearmonth datetype in ISO 8601 format
-	 * @returns {object} of year, month, timestamp properties as a number
-	 * @throws {Error}
-	 *             if the datetype is not valid
-	 * 
-	 */
-	splitGYearMonth : function(value) {
-
 		var gYearMonthExpression = new RegExp("^" + Jsonix.Schema.XSD.Calendar.GYEAR_MONTH_PATTERN + "$");
 		var results = value.match(gYearMonthExpression);
-
 		if (results !== null) {
-			var splitedGYearMonth = {
+			var gYearMonth = {
 				year : parseInt(results[1], 10),
 				month : parseInt(results[5], 10),
-				timezone : this.parseTimeZoneString(results[7])
+				timezone : this.parseTimeZoneString(results[7]),
+				date : this.xmlCalendarToDate(results[1], results[5], "01", "00", "00", "00", results[7])
 			};
+			return gYearMonth;
+		}
+		throw new Error('Value [' + value + '] doesn\'t match the gYearMonth pattern.');
+	},
 
-			return splitedGYearMonth;
+	print : function(value, context, input, scope) {
+		Jsonix.Util.Ensure.ensureObject(value);
+		var year = undefined;
+		var month = undefined;
+		var timezone = undefined;
+
+		if (value instanceof Date) {
+			year = value.getFullYear();
+			month = value.getMonth + 1;
+			timezone = value.getTimezoneOffset() * -1;
+		} else {
+			Jsonix.Util.Ensure.ensureInteger(value.year);
+			year = value.year;
+			month = value.month;
+			timezone = value.timezone;
 		}
 
-		throw new Error('Value [' + value + '] doesn\'t match the gYear pattern.');
+		// TODO: validation_issue (month range)
+		this.validateMonthRange(month);
+
+		// TODO: validation_issue (timezone range)
+		this.validateTimeZoneRange(timezone);
+
+		// TODO: validation_issue (signedYear)
+		return this.printSignedYear(year) + "-" + this.printMonth(month) + this.printTimeZoneString(timezone);
 	}
 
 });
