118bc80d73c4306a428d7b585b8adeb552c5bfc7
highsource <aleksei.valikov@gmail.com>
2015-06-20 10:00:51 +0200

    Issue #73. Added a few more assertions.

--- nodejs/scripts/tests/GH73/GH73GDay.js
+++ nodejs/scripts/tests/GH73/GH73GDay.js
@@ -13,26 +13,38 @@ module.exports = {
 		test.equal('---31', g.print({ day : 31 }));
 		test.equal('---21-05:00', g.print({ day : 21, timezone : -300 }));
 		test.equal('---21Z', g.print({ day : 21, timezone : 0 }));
+		test.equal('---21+13:59', g.print({ day : 21, timezone : 839 }));
+		test.equal('---21-14:00', g.print({ day : 21, timezone : -840 }));
 
 		test.throws(function() {
 			g.print({
 				day : 32
 			});
 		});
+
 		test.throws(function() {
 			g.print({
 				day : -1
 			});
 		});
+
 		test.throws(function() {
 			g.print({
 				day : 21,
-				timezone : '-27:00'
+				timezone : 841
+			});
+		});
+
+		test.throws(function() {
+			g.print({
+				day : 21,
+				timezone : -1841
 			});
 		});
 
 		test.done();
 	},
+
 	"PrintDayFromDate" : function(test) {
 		var g = Jsonix.Schema.XSD.GDay.INSTANCE;
 		var gDateType = new Date();
