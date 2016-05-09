5adf4450bacf1701e9e64d0952b8c53f1a6d7ea1
highsource <aleksei.valikov@gmail.com>
2016-05-09 20:41:30 +0200

    Fixes #141

--- nodejs/scripts/tests/GH141/GH141.js
+++ nodejs/scripts/tests/GH141/GH141.js
@@ -1,61 +1,58 @@
 var Jsonix = require("../../jsonix").Jsonix;
-var GH141 = require("./Mappings.js").GH141;
 
 module.exports = {
   "DurationP" : function(test) {
-    var context = new Jsonix.Context([GH141]);
-    var unmarshaller = context.createUnmarshaller();
-    var marshaller = context.createMarshaller();
+    var t = Jsonix.Schema.XSD.Duration.INSTANCE;
 
-    var P0Y0M0DT0H0M0S = unmarshaller.unmarshalString('<durationTest xmlns="gh:141"><d xmlns="">P0Y0M0DT0H0M0S</d></durationTest>');
-    test.deepEqual(P0Y0M0DT0H0M0S.value.d, { years: 0, months: 0, days: 0, hours: 0, minutes: 0, seconds: 0 });
-    var P0Y0M0DT0H0M0Sxml = marshaller.marshalString(P0Y0M0DT0H0M0S);
-    test.equal(P0Y0M0DT0H0M0Sxml, '<p0:durationTest xmlns:p0="gh:141"><d>P0Y0M0DT0H0M0S</d></p0:durationTest>');
+    var P0Y0M0DT0H0M0S = t.parse('P0Y0M0DT0H0M0S');
+    test.deepEqual(P0Y0M0DT0H0M0S, { years: 0, months: 0, days: 0, hours: 0, minutes: 0, seconds: 0 });
+    var P0Y0M0DT0H0M0SAsString = t.print(P0Y0M0DT0H0M0S);
+    test.equal(P0Y0M0DT0H0M0SAsString, 'P0Y0M0DT0H0M0S');
 
-    var P0M0DT0H0M0S = unmarshaller.unmarshalString('<durationTest xmlns="gh:141"><d xmlns="">P0M0DT0H0M0S</d></durationTest>');
-    test.deepEqual(P0M0DT0H0M0S.value.d, { months: 0, days: 0, hours: 0, minutes: 0, seconds: 0 });
-    var P0M0DT0H0M0Sxml = marshaller.marshalString(P0M0DT0H0M0S);
-    test.equal(P0M0DT0H0M0Sxml, '<p0:durationTest xmlns:p0="gh:141"><d>P0M0DT0H0M0S</d></p0:durationTest>');
+    var P0M0DT0H0M0S = t.parse('P0M0DT0H0M0S');
+    test.deepEqual(P0M0DT0H0M0S, { months: 0, days: 0, hours: 0, minutes: 0, seconds: 0 });
+    var P0M0DT0H0M0SAsString = t.print(P0M0DT0H0M0S);
+    test.equal(P0M0DT0H0M0SAsString, 'P0M0DT0H0M0S');
 
-    var P0Y0DT0H0M0S = unmarshaller.unmarshalString('<durationTest xmlns="gh:141"><d xmlns="">P0Y0DT0H0M0S</d></durationTest>');
-    test.deepEqual(P0Y0DT0H0M0S.value.d, { years: 0, days: 0, hours: 0, minutes: 0, seconds: 0 });
-    var P0Y0DT0H0M0Sxml = marshaller.marshalString(P0Y0DT0H0M0S);
-    test.equal(P0Y0DT0H0M0Sxml, '<p0:durationTest xmlns:p0="gh:141"><d>P0Y0DT0H0M0S</d></p0:durationTest>');
+    var P0Y0DT0H0M0S = t.parse('P0Y0DT0H0M0S');
+    test.deepEqual(P0Y0DT0H0M0S, { years: 0, days: 0, hours: 0, minutes: 0, seconds: 0 });
+    var P0Y0DT0H0M0SAsString = t.print(P0Y0DT0H0M0S);
+    test.equal(P0Y0DT0H0M0SAsString, 'P0Y0DT0H0M0S');
 
-    var P0Y0MT0H0M0S = unmarshaller.unmarshalString('<durationTest xmlns="gh:141"><d xmlns="">P0Y0MT0H0M0S</d></durationTest>');
-    test.deepEqual(P0Y0MT0H0M0S.value.d, { years: 0, months: 0, hours: 0, minutes: 0, seconds: 0 });
-    var P0Y0MT0H0M0Sxml = marshaller.marshalString(P0Y0MT0H0M0S);
-    test.equal(P0Y0MT0H0M0Sxml, '<p0:durationTest xmlns:p0="gh:141"><d>P0Y0MT0H0M0S</d></p0:durationTest>');
+    var P0Y0MT0H0M0S = t.parse('P0Y0MT0H0M0S');
+    test.deepEqual(P0Y0MT0H0M0S, { years: 0, months: 0, hours: 0, minutes: 0, seconds: 0 });
+    var P0Y0MT0H0M0SAsString = t.print(P0Y0MT0H0M0S);
+    test.equal(P0Y0MT0H0M0SAsString, 'P0Y0MT0H0M0S');
 
-    var P0Y0M0DT0M0S = unmarshaller.unmarshalString('<durationTest xmlns="gh:141"><d xmlns="">P0Y0M0DT0M0S</d></durationTest>');
-    test.deepEqual(P0Y0M0DT0M0S.value.d, { years: 0, months: 0, days: 0, minutes: 0, seconds: 0 });
-    var P0Y0M0DT0M0Sxml = marshaller.marshalString(P0Y0M0DT0M0S);
-    test.equal(P0Y0M0DT0M0Sxml, '<p0:durationTest xmlns:p0="gh:141"><d>P0Y0M0DT0M0S</d></p0:durationTest>');
+    var P0Y0M0DT0M0S = t.parse('P0Y0M0DT0M0S');
+    test.deepEqual(P0Y0M0DT0M0S, { years: 0, months: 0, days: 0, minutes: 0, seconds: 0 });
+    var P0Y0M0DT0M0SAsString = t.print(P0Y0M0DT0M0S);
+    test.equal(P0Y0M0DT0M0SAsString, 'P0Y0M0DT0M0S');
 
-    var P0Y0M0DT0H0S = unmarshaller.unmarshalString('<durationTest xmlns="gh:141"><d xmlns="">P0Y0M0DT0H0S</d></durationTest>');
-    test.deepEqual(P0Y0M0DT0H0S.value.d, { years: 0, months: 0, days: 0, hours: 0, seconds: 0 });
-    var P0Y0M0DT0H0Sxml = marshaller.marshalString(P0Y0M0DT0H0S);
-    test.equal(P0Y0M0DT0H0Sxml, '<p0:durationTest xmlns:p0="gh:141"><d>P0Y0M0DT0H0S</d></p0:durationTest>');
+    var P0Y0M0DT0H0S = t.parse('P0Y0M0DT0H0S');
+    test.deepEqual(P0Y0M0DT0H0S, { years: 0, months: 0, days: 0, hours: 0, seconds: 0 });
+    var P0Y0M0DT0H0SAsString = t.print(P0Y0M0DT0H0S);
+    test.equal(P0Y0M0DT0H0SAsString, 'P0Y0M0DT0H0S');
 
-    var P0Y0M0DT0H0M = unmarshaller.unmarshalString('<durationTest xmlns="gh:141"><d xmlns="">P0Y0M0DT0H0M</d></durationTest>');
-    test.deepEqual(P0Y0M0DT0H0M.value.d, { years: 0, months: 0, days: 0, hours: 0, minutes: 0 });
-    var P0Y0M0DT0H0Mxml = marshaller.marshalString(P0Y0M0DT0H0M);
-    test.equal(P0Y0M0DT0H0Mxml, '<p0:durationTest xmlns:p0="gh:141"><d>P0Y0M0DT0H0M</d></p0:durationTest>');
+    var P0Y0M0DT0H0M = t.parse('P0Y0M0DT0H0M');
+    test.deepEqual(P0Y0M0DT0H0M, { years: 0, months: 0, days: 0, hours: 0, minutes: 0 });
+    var P0Y0M0DT0H0MAsString = t.print(P0Y0M0DT0H0M);
+    test.equal(P0Y0M0DT0H0MAsString, 'P0Y0M0DT0H0M');
 
-    var P0Y0M0D = unmarshaller.unmarshalString('<durationTest xmlns="gh:141"><d xmlns="">P0Y0M0D</d></durationTest>');
-    test.deepEqual(P0Y0M0D.value.d, { years: 0, months: 0, days: 0 });
-    var P0Y0M0Dxml = marshaller.marshalString(P0Y0M0D);
-    test.equal(P0Y0M0Dxml, '<p0:durationTest xmlns:p0="gh:141"><d>P0Y0M0D</d></p0:durationTest>');
+    var P0Y0M0D = t.parse('P0Y0M0D');
+    test.deepEqual(P0Y0M0D, { years: 0, months: 0, days: 0 });
+    var P0Y0M0DAsString = t.print(P0Y0M0D);
+    test.equal(P0Y0M0DAsString, 'P0Y0M0D');
 
-    var P0Y = unmarshaller.unmarshalString('<durationTest xmlns="gh:141"><d xmlns="">P0Y</d></durationTest>');
-    test.deepEqual(P0Y.value.d, { years: 0 });
-    var P0Yxml = marshaller.marshalString(P0Y);
-    test.equal(P0Yxml, '<p0:durationTest xmlns:p0="gh:141"><d>P0Y</d></p0:durationTest>');
+    var P0Y = t.parse('P0Y');
+    test.deepEqual(P0Y, { years: 0 });
+    var P0YAsString = t.print(P0Y);
+    test.equal(P0YAsString, 'P0Y');
 
-    var PT0S = unmarshaller.unmarshalString('<durationTest xmlns="gh:141"><d xmlns="">PT0S</d></durationTest>');
-    test.deepEqual(PT0S.value.d, { seconds: 0 });
-    var PT0Sxml = marshaller.marshalString(PT0S);
-    test.equal(PT0Sxml, '<p0:durationTest xmlns:p0="gh:141"><d>PT0S</d></p0:durationTest>');
+    var PT0S = t.parse('PT0S');
+    test.deepEqual(PT0S, { seconds: 0 });
+    var PT0SAsString = t.print(PT0S);
+    test.equal(PT0SAsString, 'PT0S');
 
     test.done();
   }
